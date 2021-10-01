a5c725be01150e6fed851dab86dc28d381399024
Mario Rodríguez <marrodgar62@alum.us.es>
2021-10-01 18:30:28 +0200

    Smith4

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -33,7 +33,7 @@ class ValidatorTests {
 		person.setLastName("smith");
 		Person person1 = new Person();
 		person.setFirstName("");
-		person.setLastName("smith3");
+		person.setLastName("smith4");
 
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
