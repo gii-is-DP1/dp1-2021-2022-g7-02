caea5881dc854565d81a354d9fc41efe97adc2ad
Pablo <pabesphoy@alum.us.es>
2021-10-20 22:06:38 +0200

    Disease movido a model

--- src/main/java/org/springframework/samples/petclinic/model/Disease.java
+++ src/main/java/org/springframework/samples/petclinic/model/Disease.java
@@ -1,11 +1,9 @@
-package org.springframework.samples.petclinic.disease;
+package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-
-import org.springframework.samples.petclinic.model.NamedEntity;
 
 
 @Entity
