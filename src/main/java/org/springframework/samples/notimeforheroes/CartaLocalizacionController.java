71b5c643feda056f4b60b5a921c5da48532ccdab
Pablo <pabesphoy@alum.us.es>
2021-10-22 11:10:33 +0200

    Vista CartaLocalizacion creada con errores

---
+++ src/main/java/org/springframework/samples/notimeforheroes/CartaLocalizacionController.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.notimeforheroes;
+
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.user.AuthoritiesService;
+import org.springframework.samples.petclinic.user.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+    @Controller
+    @RequestMapping("/cartasLocalizacion")
+    public class CartaLocalizacionController {
+
+
+        private final CartaLocalizacionService cartaLocalizacionService;
+
+        @Autowired
+        public CartaLocalizacionController(CartaLocalizacionService CartaLocalizacionService, UserService userService, AuthoritiesService authoritiesService) {
+            this.cartaLocalizacionService = CartaLocalizacionService;
+        }
+
+        @GetMapping()
+        public String showCartaLocalizacionList(ModelMap modelMap) {
+            Collection<CartaLocalizacion> cartasLocalizacion = cartaLocalizacionService.findAll();
+            modelMap.addAttribute("Carta Localizacion", cartasLocalizacion);
+
+            return "cartasLocalizacion/CartaLocalizacionListing";
+        }
+
+
+    }
