f32d766955dcb101805dae34ecbf8d3d731589e0
Pablo <pabesphoy@alum.us.es>
2021-10-22 10:13:34 +0200

    CartaLocalizacion terminada

--- src/main/java/org/springframework/samples/notimeforheroes/CartaLocalizacion.java
+++ src/main/java/org/springframework/samples/notimeforheroes/CartaLocalizacion.java
@@ -7,22 +7,19 @@ import javax.validation.constraints.NotEmpty;
 
 import org.springframework.samples.petclinic.model.NamedEntity;
 
-public class CartaLocalizacion {
-	
-	@Entity
-	@Table(name="Carta Localizacion")
-	public class Disease extends NamedEntity{
-
-	    @Column(name="description",columnDefinition = "LONGTEXT")
-	    @NotEmpty
-	    private String description;
 
 
-	    public String getDescription() {
-	        return description;
-	    }
-	}
-	
-	
+@Entity
+@Table(name="cartalocalizacion")
+public class CartaLocalizacion extends NamedEntity{
 
-}
+    @Column(name="description",columnDefinition = "LONGTEXT")
+    @NotEmpty
+    private String description;
+
+
+    public String getDescription() {
+            return description;
+        }
+
+    }
