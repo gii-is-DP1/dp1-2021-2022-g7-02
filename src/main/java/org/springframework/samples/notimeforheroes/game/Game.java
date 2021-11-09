05abb8920c349475f71bd8ae18320ca411058279
Pablo <pabesphoy@alum.us.es>
2021-11-09 14:51:49 +0100

    Pruebas unitarias de games añadidas

--- src/main/java/org/springframework/samples/notimeforheroes/game/Game.java
+++ src/main/java/org/springframework/samples/notimeforheroes/game/Game.java
@@ -51,6 +51,58 @@ public class Game{
 	
 	@Column(name="isInProgress", columnDefinition = "INT")
 	private Integer isInProgress = 1;						// 1 = está en progreso. 0 = no está en progreso.
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", creator=" + creator + ", duration=" + duration + ", date=" + date
+				+ ", isInProgress=" + isInProgress + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creator == null) ? 0 : creator.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((duration == null) ? 0 : duration.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((isInProgress == null) ? 0 : isInProgress.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (creator == null) {
+			if (other.creator != null)
+				return false;
+		} else if (!creator.equals(other.creator))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (duration == null) {
+			if (other.duration != null)
+				return false;
+		} else if (!duration.equals(other.duration))
+			return false;
+		if (id != other.id)
+			return false;
+		if (isInProgress == null) {
+			if (other.isInProgress != null)
+				return false;
+		} else if (!isInProgress.equals(other.isInProgress))
+			return false;
+		return true;
+	}
 	
 	
 	
