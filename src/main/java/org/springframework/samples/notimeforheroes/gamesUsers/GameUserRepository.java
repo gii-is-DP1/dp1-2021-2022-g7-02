b324e7e1e37dd99f9710219cbf3ca20c4d6e5ab6
Miguel Angel Romalde <migromdor@alum.us.es>
2021-12-08 14:45:56 +0100

    Quitado una linea

---
+++ src/main/java/org/springframework/samples/notimeforheroes/gamesUsers/GameUserRepository.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.notimeforheroes.gamesUsers;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameUserRepository extends CrudRepository<GameUser, Integer> {
+
+    Collection<GameUser> findAll();
+
+    @Query(nativeQuery = true, value = "SELECT * FROM games_users gu WHERE gu.fk_game=?1 AND gu.fk_user=?2")
+    Optional<GameUser> findByGameAndUser(Integer gameId, Integer userId);
+
+    @Query(nativeQuery = true, value = "SELECT heroe_id FROM games_users gu WHERE gu.fk_game=?1 AND gu.fk_user=?2")
+    Optional<Integer> findHeroeOfGameUser(Integer gameId, Integer userId);
+    @Query(nativeQuery = true, value = "SELECT heroe_id FROM games_users gu WHERE gu.fk_game=?1 AND heroe_id is not null")
+    Collection<Integer> findHeroesSelectedOfGameUser(Integer gameId);
+}
