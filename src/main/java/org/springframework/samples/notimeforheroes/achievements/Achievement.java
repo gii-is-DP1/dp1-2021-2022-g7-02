fa6d4ce8b2ff4438af0a7f419e5d13f564e9c4fc
Ramon Rodriguez <ramrodbej@alum.us.es>
2022-01-07 17:18:25 +0100

    relacion achievements user

--- src/main/java/org/springframework/samples/notimeforheroes/achievements/Achievement.java
+++ src/main/java/org/springframework/samples/notimeforheroes/achievements/Achievement.java
@@ -1,10 +1,16 @@
 package org.springframework.samples.notimeforheroes.achievements;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.samples.notimeforheroes.cards.marketcard.MarketCard;
 import org.springframework.samples.petclinic.model.NamedEntity;
 
 import lombok.Getter;
@@ -19,5 +25,11 @@ public class Achievement extends NamedEntity{
 	@Column(name="description",columnDefinition = "LONGTEXT")
     @NotEmpty
 	private String description;
+	
+	@ManyToMany
+	@JoinTable(name = "achievements_users",
+		joinColumns = {@JoinColumn(name = "fk_achievement")},
+		inverseJoinColumns = {@JoinColumn(name = "fk_users")})
+	private List<Achievement> achievements;	
 	
 }
