e9e54d39f884ecf21bf39ae3bca1d60a37445fec
Javier <javmarjae@gmail.com>
2022-01-20 18:47:24 +0100

    Arreglos
    Co-authored-by: Mario Rodríguez García <marrodgar62@users.noreply.github.com>
    Co-authored-by: ramoonrb <ramoonrb@users.noreply.github.com>

--- src/main/java/org/springframework/samples/notimeforheroes/errors/CustomErrorController.java
+++ src/main/java/org/springframework/samples/notimeforheroes/errors/CustomErrorController.java
@@ -8,36 +8,35 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 
 @Controller
 public class CustomErrorController implements ErrorController{
     
-    @Override
-    public String getErrorPath(){
-        return "/error";
-    }    
-
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request){
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-    
-      if (status != null) {
-        int statusCode = Integer.parseInt(status.toString());
-        // display specific error page
-        if (statusCode == HttpStatus.NOT_FOUND.value()) {
-            return "error-404";
-        } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-            return "error-500";
-        } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
-            return "error-403";
+        @Override
+        public String getErrorPath() {
+            return "/error";
         }
-    }
-      // display generic error
-        return "error";     
-    }
 
+        @RequestMapping("/error")
+        public String handleError(HttpServletRequest request, Exception ex) {
+
+            Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+            if (status != null) {
+                int statusCode = Integer.parseInt(status.toString());
+                request.setAttribute("exception", "Excepción: " + ex.getMessage() + " -- " + ex.getCause());
+                request.setAttribute("errorcode", "Codigo de error: " + status.toString());
+                if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                    return "error-404";
+                } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                    return "error-500";
+                } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                    return "error-403";
+                }
+            }
+            return "error";
+        }
+    }*/
 
 
 
@@ -49,4 +48,4 @@ public class CustomErrorController implements ErrorController{
 
 
     
-}*/
+
--- src/main/java/org/springframework/samples/notimeforheroes/game/GameService.java
+++ src/main/java/org/springframework/samples/notimeforheroes/game/GameService.java
@@ -186,8 +186,8 @@ public class GameService {
 		}
 
 	}
-	
 
+	
 	//
 	@Transactional
 	public void createGame(@Valid Game game) {
@@ -621,11 +621,17 @@ public class GameService {
 				: (users.indexOf(game.getUserPlaying()) + 1);
 		User newUser = users.get(newIndex);
 		// Skippea el jugador si está muerto
-		while (gameUserService.findByGameAndUser(game, newUser).get().getHeroeHealth() <= 0) {
-			newIndex = (users.indexOf(game.getUserPlaying()) + 1) >= users.size() ? 0
-					: (users.indexOf(game.getUserPlaying()) + 1);
+		//while (gameUserService.findByGameAndUser(game, newUser).get().getHeroeHealth() <= 0) {
+		//	newIndex = (users.indexOf(game.getUserPlaying()) + 1) >= users.size() ? 0
+		//			: (users.indexOf(game.getUserPlaying()) + 1);
+		//}
+		if(gameUserService.findByGameAndUser(game, newUser).get().getHeroeHealth()<=0){
+			if(users.indexOf(game.getUserPlaying()) + 1 >= users.size()){
+				newUser = users.get(0);
+			}else{
+				newUser = users.get(newIndex);
+			} 
 		}
-		newUser = users.get(newIndex);
 		game.setUserPlaying(newUser);
 		// Rellena las cartas del nuevo jugador
 
